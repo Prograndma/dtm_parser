f1d8cebb98b5f2f92091060ff48e010b03e0f195
Prograndma <thomasvcole@gmail.com>
2024-09-12 15:53:17 -0600

    Preparing for creating a Mario Kart dataset.

--- dtm_parser/hugging_face.py
+++ dtm_parser/hugging_face.py
@@ -1,7 +1,11 @@
 from datasets import load_dataset
+from frame_scraper import FrameScraper
 
-dataset = load_dataset("imagefolder", data_dir="dump\\balanced")
+
+data_dir = FrameScraper.get_project_root_dir()
+data_dir = data_dir / "dtm_parser" / "dump" / "balanced"
+dataset = load_dataset("imagefolder", data_dir=f"{data_dir}")
 
 print("made it")
 
-dataset.push_to_hub("tomc43841/public_smash_balanced_dataset", private=False)
+dataset.push_to_hub("tomc43841/public_mario_kart_balanced_dataset", private=False)
