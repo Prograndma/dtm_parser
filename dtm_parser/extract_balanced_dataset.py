8f7a001303330877e58216f06c93ce356b126fa9
Prograndma <thomasvcole@gmail.com>
2024-09-12 15:52:53 -0600

    Updating paths to use pathlib for increased robustness.

--- dtm_parser/extract_balanced_dataset.py
+++ dtm_parser/extract_balanced_dataset.py
@@ -2,6 +2,7 @@ import pandas as pd
 from controller_data import ControllerData
 import cv2
 import os
+from frame_scraper import FrameScraper
 
 
 def extract_row_into_objects(row):
@@ -26,8 +27,8 @@ def extract_row_into_objects(row):
     return controller_data, row.file_name
 
 
-def main(where_get, where_save):
-    inputs = pd.read_csv(f"{where_get}metadata.csv")
+def main(initial_location, balanced_save_location):
+    inputs = pd.read_csv(f"{initial_location / 'metadata'}.csv")
 
     controller_datas = []
     image_names = []
@@ -37,16 +38,16 @@ def main(where_get, where_save):
             controller_datas.append(controller_data)
             image_names.append(image_file_name)
 
-    if not os.path.exists(f"{where_save}"):
-        os.makedirs(f"{where_save}")
+    if not os.path.exists(f"{balanced_save_location}"):
+        os.makedirs(f"{balanced_save_location}")
 
     for i, image_name in enumerate(image_names):
         if i % 10000 == 0:
             print(f"Copying {i}/{len(image_names)} images...")
-        image = cv2.imread(f"{where_get}{image_name}")
+        image = cv2.imread(f"{initial_location / image_name}")
 
         if image is not None:
-            saved = cv2.imwrite(f"{where_save}{image_name}", image)
+            saved = cv2.imwrite(f"{balanced_save_location / image_name}", image)
             if saved:
                 pass
             else:
@@ -57,7 +58,7 @@ def main(where_get, where_save):
     objects_data = [{"file_name": image_name, **vars(obj)} for image_name, obj in zip(image_names, controller_datas)]
     df = pd.DataFrame(objects_data)
 
-    df.to_csv(f'{where_save}metadata.csv', index=False)
+    df.to_csv(f'{balanced_save_location / "metadata"}.csv', index=False)
 
     print(f"Amount of un-interesting rows: {inputs.shape[0] - len(image_names)}")
     print(f"Amount of interesting images : {len(image_names)}")
@@ -65,4 +66,7 @@ def main(where_get, where_save):
 
 
 if __name__ == "__main__":
-    main("dump\\big\\", "dump\\balanced\\")
+    root_path = FrameScraper.get_project_root_dir()
+    where_get = root_path / "dtm_parser" / "dump" / "big"
+    where_save = root_path / "dtm_parser" / "dump" / "balanced"
+    main(where_get, where_save)
