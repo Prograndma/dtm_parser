227911f2f820c976fa40d8192290dba79c12f943
Thomas Cole <47041970+Prograndma@users.noreply.github.com>
2025-12-07 10:20:19 -0700

    Update hugging_face.py
    just old local changes.

--- dtm_parser/hugging_face.py
+++ dtm_parser/hugging_face.py
@@ -1,11 +1,43 @@
 from datasets import load_dataset
 from frame_scraper import FrameScraper
+from my_secrets import HF_TOKEN
 
 
-data_dir = FrameScraper.get_project_root_dir()
-data_dir = data_dir / "dtm_parser" / "dump" / "balanced"
-dataset = load_dataset("imagefolder", data_dir=f"{data_dir}")
+def push_dataset(balanced, number):
+    data_dir = FrameScraper.get_project_root_dir()
 
-print("made it")
+    if balanced:
+        ds_type = "balanced"
+        data_dir = data_dir / "dtm_parser" / "dump" / f"{number}_balanced"
+    else:
+        ds_type = "full"
+        data_dir = data_dir / "dtm_parser" / "dump" / number
 
-dataset.push_to_hub("tomc43841/public_mario_kart_balanced_dataset", private=False)
+
+    dataset = load_dataset(f"{data_dir}")
+
+    print("made it")
+
+    dataset.push_to_hub(f"tomc43841/public_smash_{ds_type}_dataset_{number}", private=False, token=HF_TOKEN)
+
+
+# push_dataset(True, "01")
+# push_dataset(False, "01")
+
+# push_dataset(True, "02")
+# push_dataset(False, "02")
+
+# push_dataset(True, "03")
+# push_dataset(False, "03")
+
+# push_dataset(True, "04")
+# push_dataset(False, "04")
+#
+# push_dataset(True, "05")
+# push_dataset(False, "05")
+
+push_dataset(True, "06")
+push_dataset(False, "06")
+
+push_dataset(True, "07")
+push_dataset(False, "07")
